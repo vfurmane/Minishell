08af99bcb1b0e3da8fc020da4d414d5057becf98
reaster <earnaud@student.42.fr>
2021-03-30 14:47:20 +0200

    added some headers and ft_Strlen and a basic echo.c

---
+++ includes/builtin.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/30 14:25:10 by earnaud           #+#    #+#             */
+/*   Updated: 2021/03/30 14:36:58 by earnaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUILTIN_H
+# define BUILTIN_H
+# include "minishell.h"
+# include <linux/limits.h>
+
+
+int ft_echo(int n, char *str, int fd);
+
+
+#endif
---
+++ includes/minishell.h
@@ -0,0 +1,11 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <linux/limits.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "builtin.h"
+
+size_t ft_strlen(const char *str);
+
+#endif
