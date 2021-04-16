552d7c2c30b3c892ab819e73427a7df92a6d6635
vfurmane <vfurmane@student.42.fr>
2021-04-16 08:33:44 +0200

    Handle all letters, and display a message on arrow key press

--- main.c
+++ main.c
@@ -15,6 +15,10 @@ int	main(void)
 	int				ret;
 	unsigned char	buffer[3];
 	char			*cm, *DO, *UP, *LE, *RI;
+	char			*arrow[4] = {	"UP",
+									"DOWN",
+									"RIGHT",
+									"LEFT"};
 
 	tgetent(NULL, getenv("TERM"));
 	cm = tgetstr("cm", NULL);
@@ -31,23 +35,26 @@ int	main(void)
 		ret = read(0, buffer, 3);
 		if (buffer[0] >= 32 && buffer[0] <= 126)
 		{
-			//ft_putchar(buffer);
+			ft_putchar(buffer[0]);
 			//tputs(tgoto(RI, 0, 1), 1, ft_putchar);
 		}
 		else if (buffer[0] == 127)
 		{
-			//tputs(tgoto(LE, 0, 1), 1, ft_putchar);
+			tputs(tgoto(LE, 0, 1), 1, ft_putchar);
+			ft_putchar(' ');
+			tputs(tgoto(LE, 0, 1), 1, ft_putchar);
 		}
 		else if (buffer[0] == 27 && buffer[1] == 91)
 		{
-			if (buffer[2] == 'A')
+			/*if (buffer[2] == 'A')
 				ft_putchar('^');
 			else if (buffer[2] == 'B')
 				ft_putchar('v');
 			else if (buffer[2] == 'C')
 				ft_putchar('>');
 			else if (buffer[2] == 'D')
-				ft_putchar('<');
+				ft_putchar('<');*/
+			printf("\nHave you pressed %s key ?\n", arrow[buffer[2] - 'A']);
 		}
 	}
 	termios_c.c_lflag &= (ICANON | ECHO);
