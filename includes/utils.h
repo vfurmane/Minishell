a30c7bc9ccfda6013ea95fca8898f81c6a84b7d8
vfurmane <vfurmane@student.42.fr>
2021-04-20 13:57:47 +0200

    Clean the .h file

--- includes/utils.h
+++ includes/utils.h
@@ -6,7 +6,7 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 13:42:36 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/04/20 09:19:24 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/04/20 13:51:13 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,26 @@ void				*ft_lstadd_back(void *lst, void *new);
 void				*ft_lstlast(void *lst);
 int					ft_lstsize(void *lst);
 void				*ft_memcpy(void *dest, const void *src, size_t len);
+void				*ft_memset(void *str, int c, size_t n);
 int					ft_putchar(int chr);
+char				**ft_split(char const *s, char c);
+/* splitstr = splitarr ? */
+char				**ft_strarrdup(char **arr);
 int					ft_strccmp(const char *str1, const char *str2, int chr);
 char				*ft_strcdup(const char *str, int chr);
 char				*ft_strchr(const char *str, int chr);
 char				*ft_strcidup(const char *str, int chr);
-char				**ft_strarrdup(char **arr);
 int					ft_strcmp(const char *str1, const char *str2);
+size_t				ft_strcpy(char *dest, const char *src);
 char				*ft_strdup(const char *str);
 char				*ft_strinstr(const char *str, const char *sep);
+char				*ft_strjoin(char const *s1, char const *s2);
+size_t				ft_strlcpy(char *dest, const char *src, size_t size);
 size_t				ft_strlen(const char *str);
 char				*ft_strndup(const char *str, int len);
 char				*ft_strnstr(const char *big,
 					const char *little, size_t len);
+char				*ft_substr(char const *s, unsigned int start, size_t len);
 
 
 #endif
