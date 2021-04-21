5da1b4a202f9723a04771a2e52291d0dc0539719
vfurmane <vfurmane@student.42.fr>
2021-04-21 10:29:48 +0200

    Display ^C on ctrl + C

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 17:09:18 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/04/21 09:19:54 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/04/21 10:29:06 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -136,10 +136,11 @@ int	main(int argc, char **argv, char **envp)
 		if (fork() != CHILD_PROCESS)
 		{
 			wait(&status);
+			tcsetattr(0, 0, &shell_c.termios_backup);
 			if (WTERMSIG(status) == 42)
 				chdir("..");
 			if (WTERMSIG(status) == SIGINT)
-				write(1, "\n", 1);
+				write(1, "^C\n", 3);
 			else
 				ft_update_shell(&shell_c);
 		}
