e939c4ba047188150daf756923107b6608f41cf0
vfurmane <vfurmane@student.42.fr>
2021-05-04 11:29:10 +0200

    Use ft_write_pipe in ft_exit

--- srcs/builtin/ft_exit.c
+++ srcs/builtin/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 12:09:19 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/04/21 10:52:03 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/05/04 11:27:28 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ int ft_exit(char **args, int fd, t_config *shell_c)
 	(void)args;
 	(void)fd;
 	write(fd, "exit\n", 5);
-	write(shell_c->fd[1], EXIT_SHELL, 3);
-	write(shell_c->fd[1], "\x1F\x1E", 2);
+	ft_write_pipe(EXIT_SHELL, NULL, NULL, shell_c->fd[1]);
 	return (0);
 }
