49d6112ac64db1f0781dc7a8cfe4a567bf20d7b1
vfurmane <vfurmane@student.42.fr>
2021-05-10 16:48:33 +0200

    Check the identifier for unset

--- srcs/builtin/unset.c
+++ srcs/builtin/unset.c
@@ -6,11 +6,22 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/01 12:08:49 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/05/09 11:52:43 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/05/10 16:47:40 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
+
+static int	ft_is_valid_identifier(const char *arg)
+{
+	int	i;
+
+	i = 0;
+	while (arg[i] && arg[i] != '=' && (ft_isalpha(arg[i]) ||
+				(ft_isdigit(arg[i]) && i > 0) || arg[i] == '_'))
+		i++;
+	return (arg[i] == '\0' && i > 0);
+}
 
 int ft_unset(t_config *shell_c, char **args, int output_fd)
 {
@@ -19,6 +30,12 @@ int ft_unset(t_config *shell_c, char **args, int output_fd)
 	(void)output_fd;
 	i = 0;
 	while (args[i])
-		ft_write_pipe(DEL_ENV, args[i++], NULL, shell_c->fd[1]);
+	{
+		if (ft_is_valid_identifier(args[i]))
+			ft_write_pipe(DEL_ENV, args[i], NULL, shell_c->fd[1]);
+		else
+			ft_stderr_message("unset: `", args[i], "': not a valid identifier", 0);
+		i++;
+	}
 	return (0);
 }
