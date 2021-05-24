69d68ba0d919a6e2577ebf282373c0589ca5d000
reaster <earnaud@student.42.fr>
2021-05-24 17:57:14 +0200

    finally i'm just dumb

--- srcs/cmd.c
+++ srcs/cmd.c
@@ -6,7 +6,7 @@
 /*   By: earnaud <earnaud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/31 18:42:16 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/05/24 17:43:15 by earnaud          ###   ########.fr       */
+/*   Updated: 2021/05/24 17:56:29 by earnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int ft_recursiv_command(t_cmd *cmd, t_config *shell_c, int pipe_in, int std_out)
 	int		ret;
 	int		status;
 	char	**args;
+	t_cmd	*cmd_next;
+	int		fd_next;
 
 	if (!cmd)
 	{
@@ -83,9 +85,13 @@ int ft_recursiv_command(t_cmd *cmd, t_config *shell_c, int pipe_in, int std_out)
 		}
 		ret = ft_route_command(args[0], &args[1], args, shell_c);
 		free_neo(args);
-		if (ret != 127)
-			ft_recursiv_command(cmd->next, shell_c, cmd->fd[0], std_out);
-		free_all_cmd(cmd);
+		free(cmd->str);
+		cmd_next = cmd->next;
+		fd_next = cmd->fd[0];
+		free(cmd);
+		//if (ret != 127)
+			ft_recursiv_command(cmd_next, shell_c, fd_next, std_out);
+		//free_all_cmd(cmd);
 		free_shell(shell_c);
 		exit (ret);
 	}
